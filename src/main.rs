b10ed265f1abc680adb83418e4553813ae42bd1d
Ryan Davis <ryandavis@deloitte.com>
2021-07-17 18:42:38 -0700

    Changed dis to use an inputted counter

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,6 @@
 pub mod disassembler;
-pub mod opcodes;
 
 fn main() {
     let code: [i32; 3] = [0x11, 0xf3, 0xd2];
-    disassembler::disassembler_8080(&code);
-    // match input {
-    //     0x32 => println!("NOP"),
-    //     _ => println!("Something else")
-    // }
+    let mut opbytes: i32 = disassembler::disassembler_8080(&code, 0);
 }
