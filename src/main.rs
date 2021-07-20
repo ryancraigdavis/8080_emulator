01a069cdc2db517d5c637d0827c3411e2cdfbf58
Ryan Davis <ryandavis@deloitte.com>
2021-07-19 19:20:13 -0700

    Attempts at reading the file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 pub mod disassembler;
+use std::fs;
+use std::path::Path;
 
 fn main() {
     let code: [i32; 3] = [0x11, 0xf3, 0xd2];
+
+    let path = Path::new("space_invaders_rom/invaders.h");
+    let contents = fs::read_to_string(path);
+
+    println!("{}", contents[0]);
+
     let mut opbytes: i32 = disassembler::disassembler_8080(&code, 0);
 }
