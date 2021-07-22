65cd6bf19ccf55a377af85bf99868cfd05890706
Gagandeep Singh <59806919+GSingh-osu@users.noreply.github.com>
2021-07-22 18:16:00 -0500

    Added initial infrastructure

---
+++ src/condition_codes.rs
@@ -0,0 +1,11 @@
+// References - http://www.emulator101.com/emulator-shell.html
+#[derive(Debug,Default)]
+pub struct ConditionCodes
+{
+    pub z: bool,
+    pub s: bool,
+    pub p: bool,
+    pub cy: bool,
+    pub ac: bool,
+    pub pad: u8
+}
---
+++ src/intel8080_state.rs
@@ -0,0 +1,17 @@
+use crate::condition_codes::ConditionCodes;
+
+// Created using http://www.emulator101.com/emulator-shell.html as a resource
+#[derive(Debug,Default)]
+pub struct StateIntel8080
+{ 
+    pub a: u8,
+    pub b: u8,
+    pub c: u8,
+    pub d: u8,
+    pub e: u8,
+    pub h: u8,
+    pub l: u8,
+    pub sp: u16,
+    pub pc: u8,
+    pub condition: ConditionCodes
+}
