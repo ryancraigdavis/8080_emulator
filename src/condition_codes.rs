b3a6a8fd6dccc6f88fc3bc9658abc4637b846a89
GSingh-osu <59806919+GSingh-osu@users.noreply.github.com>
2021-07-22 18:35:15 -0500

    Updated formatting with rustfmt

--- src/condition_codes.rs
+++ src/condition_codes.rs
@@ -1,11 +1,10 @@
 // References - http://www.emulator101.com/emulator-shell.html
-#[derive(Debug,Default)]
-pub struct ConditionCodes
-{
+#[derive(Debug, Default)]
+pub struct ConditionCodes {
     pub z: bool,
     pub s: bool,
     pub p: bool,
     pub cy: bool,
     pub ac: bool,
-    pub pad: u8
+    pub pad: u8,
 }
