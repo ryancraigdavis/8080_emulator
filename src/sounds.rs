75650dc7fb9ade1e03547236d3bea96fb1170bf6
Ryan Davis <ryandavis@deloitte.com>
2021-08-06 12:22:55 -0700

    merge

--- src/sounds.rs
+++ src/sounds.rs
@@ -1,42 +1,32 @@
-// References - https://docs.rs/rodio/0.14.0/rodio/ 
+// References - https://docs.rs/rodio/0.14.0/rodio/
 // References - https://github.com/mohanson/space-invaders/
+use rodio::source::Source;
+use std::fs::File;
+use std::io::BufReader;
+
 #[derive(Debug, Default)]
-struct Invaderwavs {
-    sink: rodio::Sink,
-    wavs: [Vec<u8>; 10],
+pub struct Invaderwavs {
+    pub sounds: Vec<String>,
 }
 
 impl Invaderwavs {
-    fn power_up(snd: impl AsRef<Path>) -> Self {
-        let res = snd.as_ref().to_path_buf();
-        let get = |x| {
-            let mut res = res.clone();
-            res.push(x);
-            std::fs::read(res).unwrap()
-        };
-        let mut wavs = [
-            vec![],
-            vec![],
-            vec![],
-            vec![],
-            vec![],
-            vec![],
-            vec![],
-            vec![],
-            vec![],
-            vec![],
-        ];
-        for i in 0..10 {
-            wavs[i as usize] = get(format!("{}.wav", i));
-        }
-        let device = rodio::default_output_device().unwrap();
-        let sink = rodio::Sink::new(&device);
-        Self { sink, wavs }
+    pub fn load_sounds(&mut self) {
+        self.sounds.push(String::from("sounds/res_snd_0.wav"));
+        self.sounds.push(String::from("sounds/res_snd_1.wav"));
+        self.sounds.push(String::from("sounds/res_snd_2.wav"));
+        self.sounds.push(String::from("sounds/res_snd_3.wav"));
+        self.sounds.push(String::from("sounds/res_snd_4.wav"));
+        self.sounds.push(String::from("sounds/res_snd_5.wav"));
+        self.sounds.push(String::from("sounds/res_snd_6.wav"));
+        self.sounds.push(String::from("sounds/res_snd_7.wav"));
+        self.sounds.push(String::from("sounds/res_snd_8.wav"));
+        self.sounds.push(String::from("sounds/res_snd_9.wav"));
     }
-
-    fn play_sound(&self, i: usize) {
-        let data = self.wavs[i].clone();
-        let cursor = Cursor::new(data);
-        self.sink.append(rodio::Decoder::new(cursor).unwrap());
+    pub fn play_sound(&self, i: usize) {
+        let stream_handle = rodio::default_output_device().unwrap();
+        let data = self.sounds[i].clone();
+        let file = File::open(data).unwrap();
+        let source = rodio::Decoder::new(BufReader::new(file)).unwrap();
+        rodio::play_raw(&stream_handle, source.convert_samples());
     }
 }
